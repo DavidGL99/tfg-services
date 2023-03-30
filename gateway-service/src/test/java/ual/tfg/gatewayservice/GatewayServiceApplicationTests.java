5ebe31602e6bd79a265f6c109d725bd7cce12e4c
dgomezl <david.parador99@gmail.com>
2023-03-30 16:32:55 +0200

    gateway service

---
+++ gateway-service/src/test/java/ual/tfg/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ual.tfg.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
